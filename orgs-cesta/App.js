388becf2559938792ad8845fe98bc21579a004bd
Italo Cavalcanti <italobc93@gmail.com>
2022-02-16 22:19:32 -0300

    adicionando Google Fonts, Montserrat

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import Cesta from './src/telas/Cesta'
 import { StatusBar, SafeAreaView, View } from 'react-native'
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat'
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_700Bold
+  })
+
+  if (!fonteCarregada) {
+    return <View />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
--- orgs-cesta/src/telas/Cesta.js
+++ orgs-cesta/src/telas/Cesta.js
@@ -51,7 +51,7 @@ const estilos = StyleSheet.create({
     color: '#464646',
     fontSize: 26,
     lineHeight: 42,
-    fontWeight: 'bold'
+    fontFamily: 'Montserrat_700Bold'
   },
   fazenda: {
     flexDirection: 'row',
@@ -64,7 +64,8 @@ const estilos = StyleSheet.create({
   nomeFazenda: {
     fontSize: 16,
     lineHeight: 26,
-    marginLeft: 12
+    marginLeft: 12,
+    fontFamily: 'Montserrat_400Regular'
   },
   descricao: {
     color: '#A3A3A3',
