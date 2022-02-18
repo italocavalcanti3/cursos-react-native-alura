133c58ba0e2383398ad0f8d97e025b356576dbda
Italo Cavalcanti <italobc93@gmail.com>
2022-02-17 23:13:59 -0300

    Substituindo map por FlatList e finalizando primeiro curso

--- orgs-cesta/src/telas/Cesta/index.js
+++ orgs-cesta/src/telas/Cesta/index.js
@@ -1,20 +1,30 @@
 import React from 'react'
-import { ScrollView, StyleSheet, View } from 'react-native'
+import { FlatList, StyleSheet, View } from 'react-native'
 
 import Topo from './componentes/Topo'
 import Detalhes from './componentes/Detalhes'
-import Itens from './componentes/Itens'
+import Item from './componentes/Item'
+import Texto from '../../componentes/Texto'
 
 export default function Cesta({ topo, detalhes, itens }) {
   return (
     <>
-      <ScrollView>
-        <Topo {...topo} />
-        <View style={estilos.cesta}>
-          <Detalhes {...detalhes} />
-          <Itens {...itens} />
-        </View>
-      </ScrollView>
+      <FlatList
+        data={itens.lista}
+        renderItem={Item}
+        keyExtractor={({ nome }) => nome}
+        ListHeaderComponent={() => {
+          return (
+            <>
+              <Topo {...topo} />
+              <View style={estilos.cesta}>
+                <Detalhes {...detalhes} />
+                <Texto style={estilos.titulo}>{itens.titulo}</Texto>
+              </View>
+            </>
+          )
+        }}
+      />
     </>
   )
 }
@@ -22,6 +32,17 @@ export default function Cesta({ topo, detalhes, itens }) {
 const estilos = StyleSheet.create({
   cesta: {
     paddingVertical: 8,
+    paddingHorizontal: 16
+  },
+  titulo: {
+    fontSize: 20,
+    lineHeight: 32,
+    color: '#464646',
+    fontWeight: 'bold',
+    marginTop: 32,
+    marginBottom: 8
+  },
+  lista: {
     paddingHorizontal: 16
   }
 })
