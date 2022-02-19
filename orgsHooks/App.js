72e36b2fd4a0ebd251859675c508aaa6ad759e36
Italo Cavalcanti <italobc93@gmail.com>
2022-02-19 11:34:32 -0300

    criando novo projeto OrgsHooks com RN CLI

---
+++ orgsHooks/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Alura</Text>
+    </SafeAreaView>
+  );
+}
+export default App;
